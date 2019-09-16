1cab948755188087d8372b4371cab2efb600155d
shumvlesu <sashumihin@gmail.com>
2019-09-16 11:36:06 +0300

    171стр.

--- app/src/main/java/com/hfad/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/hfad/criminalintent/CrimeFragment.java
@@ -8,12 +8,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 
 public class CrimeFragment extends Fragment {
     private Crime mCrime;
     private EditText mTitleField;
     private Button mDateButton;
+    private CheckBox mSolvedCheckBox;
 
 
     @Override
@@ -49,6 +52,15 @@ public class CrimeFragment extends Fragment {
         mDateButton = v.findViewById(R.id.crime_date);
         mDateButton.setText(mCrime.getDate().toString());
         mDateButton.setEnabled(false);
+
+        mSolvedCheckBox = v.findViewById(R.id.crime_solved);
+        mSolvedCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView,
+                                         boolean isChecked) {
+                mCrime.setSolved(isChecked);
+            }
+        });
 
         return v;
     }
