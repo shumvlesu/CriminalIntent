bad261c5b0a3a5f2ad970cc76b438e87c7f3d142
shumvlesu <sashumihin@gmail.com>
2019-09-18 14:49:17 +0300

    179стр.

---
+++ app/src/main/java/com/hfad/criminalintent/CrimeLab.java
@@ -0,0 +1,17 @@
+package com.hfad.criminalintent;
+
+import android.content.Context;
+
+public class CrimeLab {
+
+    private static CrimeLab sCrimeLab;
+    public static CrimeLab get(Context context) {
+        if (sCrimeLab == null) {
+            sCrimeLab = new CrimeLab(context);
+        }
+        return sCrimeLab;
+    }
+    private CrimeLab(Context context) {
+    }
+
+}
