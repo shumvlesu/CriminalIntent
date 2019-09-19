2d391b657e3826fc57e628327a6d8d769f0ee64a
shumvlesu <sashumihin@gmail.com>
2019-09-19 17:11:57 +0300

    179стр.

--- app/src/main/java/com/hfad/criminalintent/CrimeLab.java
+++ app/src/main/java/com/hfad/criminalintent/CrimeLab.java
@@ -2,9 +2,15 @@ package com.hfad.criminalintent;
 
 import android.content.Context;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
 public class CrimeLab {
 
     private static CrimeLab sCrimeLab;
+    private List<Crime> mCrimes;
+
     public static CrimeLab get(Context context) {
         if (sCrimeLab == null) {
             sCrimeLab = new CrimeLab(context);
@@ -12,6 +18,19 @@ public class CrimeLab {
         return sCrimeLab;
     }
     private CrimeLab(Context context) {
+        mCrimes = new ArrayList<>();
+    }
+
+    public List<Crime> getCrimes() {
+        return mCrimes;
+    }
+    public Crime getCrime(UUID id) {
+        for (Crime crime : mCrimes) {
+            if (crime.getId().equals(id)) {
+                return crime;
+            }
+        }
+        return null;
     }
 
 }
