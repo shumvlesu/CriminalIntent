7abd269ac2dac953596b653c6dacf65c2bc7cfd9
sergey <sashumihin@gmail.com>
2020-01-08 10:11:18 +0300

    186 стр. RecyclerView.

--- app/src/main/java/com/hfad/criminalintent/CrimeListActivity.java
+++ app/src/main/java/com/hfad/criminalintent/CrimeListActivity.java
@@ -2,11 +2,9 @@ package com.hfad.criminalintent;
 
 import android.support.v4.app.Fragment;
 
-public class CrimeActivity extends SingleFragmentActivity{
-
+public class CrimeListActivity extends SingleFragmentActivity {
     @Override
     protected Fragment createFragment() {
-       return new CrimeFragment();
-
+        return new CrimeListFragment();
     }
 }
--- app/src/main/java/com/hfad/criminalintent/SingleFragmentActivity.java
+++ app/src/main/java/com/hfad/criminalintent/SingleFragmentActivity.java
@@ -12,18 +12,15 @@ public abstract class SingleFragmentActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_crime);
+        setContentView(R.layout.activity_fragment);
 
         FragmentManager fm = getSupportFragmentManager();
         Fragment fragment = fm.findFragmentById(R.id.fragment_container);
         if (fragment == null) {
-            fragment = new createFragment();
-            fm.beginTransaction()
+           fragment = createFragment();
+           fm.beginTransaction()
                     .add(R.id.fragment_container, fragment)
                     .commit();
         }
-
     }
-
-
 }
