26f4213d34d2997fb8e9c50baf4c1667866968fb
sergey <sashumihin@gmail.com>
2020-02-15 09:06:35 +0300

    252 стр.

--- app/src/main/java/com/hfad/criminalintent/DatePickerFragment.java
+++ app/src/main/java/com/hfad/criminalintent/DatePickerFragment.java
@@ -4,12 +4,18 @@ package com.hfad.criminalintent;
 import android.app.Dialog;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
+import android.view.LayoutInflater;
+import android.view.View;
 
 public class DatePickerFragment extends android.support.v4.app.DialogFragment {
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
+        View v = LayoutInflater.from(getActivity())
+                .inflate(R.layout.dialog_date, null);
+
         return new AlertDialog.Builder(getActivity())
+                .setView(v)
                 .setTitle(R.string.date_picker_title)
                 .setPositiveButton(android.R.string.ok, null)
                 .create();
